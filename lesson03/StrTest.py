08b11e867b9cc39780ab4cd7f9f206475d0bfaf3
vincentjava <vincentjava@yahoo.com.tw>
2019-05-13 14:23:09 +0800

    python3

---
+++ lesson03/StrTest.py
@@ -0,0 +1,9 @@
+import locale
+str1 = 'Hello Python'
+print(str1[4])
+print(str1[-4])
+print(str1[1:4])  # front 1 ~ end 4 (not include)
+print(str1[1:4])  # 1 <= value < 4
+print(str1[:4])   # 0 <= value < 4
+
+print(locale.atoi('10') + 2)
