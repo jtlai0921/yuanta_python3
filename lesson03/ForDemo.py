3d82b30e5e190eb7c485dbf5613cd94bec61af21
vincentjava <vincentjava@yahoo.com.tw>
2019-05-13 15:21:59 +0800

    python3

--- lesson03/ForDemo.py
+++ lesson03/ForDemo.py
@@ -7,3 +7,11 @@ for item in [10, 20, 30]:
 
 for (i, item) in enumerate([10, 20, 30]):
     print(i, item)
+
+hey = ["lol", "hey", "water", "pepsi", "jam"]
+for item in hey:
+    print(hey.index(item), item)
+
+employees = {'Tom':70000, 'Mary':80000, 'John':55000}
+for name in employees:
+    print('%s 的薪資 %d' % (name, employees[name]))
