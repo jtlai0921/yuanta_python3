817ef5fd43770fae077ade3fd63dde9d0e70d032
vincenttuan <vincentjava@yahoo.com.tw>
2019-05-19 23:17:17 +0800

    python3

--- lesson02/CalcBMI.py
+++ lesson02/CalcBMI.py
@@ -11,7 +11,8 @@ print("{0:.2f}".format(bmi))
 
 print('h=' + str(h) + ", w=" + str(w))
 print(h, w, sep='&', end='\t')
-print(h, w, sep='&')
+print(h, w, sep="&")
 print("h=%.2f w=%03d" % (h, w))
 print("h={0} w={1}".format(h, w))
 print("h={0:.2f} w={1}".format(h, w))
+
