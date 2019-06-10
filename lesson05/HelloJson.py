8d6aae27137b12bc43915509288fe4a5661c2157
vincenttuan <vincentjava@yahoo.com.tw>
2019-06-10 15:09:20 +0800

    python3

--- lesson05/HelloJson.py
+++ lesson05/HelloJson.py
@@ -11,3 +11,9 @@ x2 = '[{"name":"John", "age":30},' \
 
 y2 = json.loads(x2)
 print(type(y2))
+for item in y2:
+    print(item['age'])
+
+
+jsonstr = json.dumps(y2, indent=2, sort_keys=True)
+print(jsonstr)
