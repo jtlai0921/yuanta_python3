ad9ccb32e23cd18633c9ff99e19377adac69f13b
vincenttuan <vincentjava@yahoo.com.tw>
2019-06-10 15:34:24 +0800

    python3

--- lesson05/BadFood_Json.py
+++ lesson05/BadFood_Json.py
@@ -2,7 +2,12 @@ import requests
 import json
 url = 'http://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
 
-def check(json, name):
+
+def check(foods, name):
+    for item in foods:
+        if name in item['品名']:
+            print(item)
+            return True
     return False
 
 
@@ -10,8 +15,8 @@ resp = requests.get(url)
 if resp.status_code == 200:
     text = resp.text
     # 輸入商品名可以知道該商品是否是黑心米 ?
-    json = json.loads(text)
-    if check(json, '壽司米'):
+    foods = json.loads(text)
+    if check(foods, '壽司米'):
         print('黑心米')
     else:
         print('安全米')
