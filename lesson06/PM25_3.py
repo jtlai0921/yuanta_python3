064e21ffd59251bc990e012b8bcfaabfbb53cbdb
vincenttuan <vincentjava@yahoo.com.tw>
2019-06-14 14:43:23 +0800

    python3

--- lesson06/PM25_3.py
+++ lesson06/PM25_3.py
@@ -8,18 +8,15 @@ headers = {
 url = 'https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=1'
 html = requests.get(url, headers=headers)
 
+
 sp = BeautifulSoup(html.text, 'html.parser')
 
-print(sp.select("title"))
-print(len(sp.select("title")));
-#
-print(sp.select("title")[0]);
-#
-print(sp.select("title")[0].text);
-#
 print(sp.select("title")[0].text.strip());
-#
+
 print(sp.find("span", {"id":"ctl08_labText1"}).text.strip());
-#
-print(sp.find("a", {"href":"HourlyData.aspx"}).get('title').strip());
+
+rs = sp.find_all("tr", {"align":"center", "style":"border-width:1px;border-style:Solid;"})
+
+for r in rs:
+    print(r.text.strip())
 
