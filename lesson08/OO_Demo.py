301ecb616e6d6592a1736e1478be7651df2aa677
vincenttuan <vincentjava@yahoo.com.tw>
2019-06-21 15:15:17 +0800

    python3

--- lesson08/OO_Demo.py
+++ lesson08/OO_Demo.py
@@ -7,6 +7,11 @@ class Human:
         return self.name + ',' + str(self.age) + ',' + self.sex
 
 
+class Student(Human):
+    number = 0
+    grade = ''
+
+
 h = Human()
 h.name = 'Vincent'
 h.age = 20
@@ -14,3 +19,15 @@ h.sex = '男'
 
 print(h.name, h.age, h.sex)
 print(h)
+
+student = Student()
+student.name = 'Vincent'
+student.age = 7
+student.sex = '男'
+student.number = 6
+student.grade = '一年級'
+print(student.name)
+print(student.age)
+print(student.sex)
+print(student.number)
+print(student.grade)
--- lesson08/OO_Demo2.py
+++ lesson08/OO_Demo2.py
@@ -2,6 +2,10 @@ class Account:
     name = '' # 公有屬性
     __balance = 0 # 私有屬性
 
+    def __init__(self, name, __balance):
+        self.name = name
+        self.__balance = __balance
+
     def setBalance(self, balance): # 物件方法，需含 self 參數
         self.__balance = balance
 
@@ -9,8 +13,5 @@ class Account:
         return self.__balance
 
 
-account = Account()
-account.name = 'Vincent'
-account.setBalance(30000)
-print(account.name)
-print(account.getBalance())
+account = Account('John', 10000)
+print(account.name, account.getBalance())
