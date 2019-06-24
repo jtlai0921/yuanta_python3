30c78a25360c2f92f158eb1962bf3735e2210dc7
vincenttuan <vincentjava@yahoo.com.tw>
2019-06-24 14:46:45 +0800

    python3

--- lesson08/SingleTon_Demo.py
+++ lesson08/SingleTon_Demo.py
@@ -5,6 +5,7 @@ class Report(object):
             self.val = 'empty'
         def __str__(self):
             return 'self:' + self.val
+
     instance = None
 
     def __new__(cls): # __new__ always a classmethod
@@ -18,20 +19,17 @@ class Report(object):
     def __setattr__(self, name):
         return setattr(self.instance, name)
 
-x = Report()
-print(x)
+# x = Report()
+# print(x)
 
 a = Report()
 a.val = 'mysql'
-print(a)
 
 b = Report()
 b.val = 'derby'
-print(b)
 
 c = Report()
 c.val = 'oracle'
-print(c)
 
 print('----------------')
 print(a, b, c)
