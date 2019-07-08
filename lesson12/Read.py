5d17931e1bf5919dd0ac3ebb73fae9e1b1bb782c
vincenttuan <vincentjava@yahoo.com.tw>
2019-07-08 14:47:23 +0800

    python3

--- lesson12/Read.py
+++ lesson12/Read.py
@@ -3,8 +3,9 @@ from lesson12.Dao import getSession, User
 if __name__ == '__main__':
     session = getSession()
 
-    for u in session.query(User):
-        print(u)
+    users = session.query(User)
+    for user in users:
+        print(user)
 
     # 關閉 session:
     session.close()
