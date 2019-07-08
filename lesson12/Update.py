9b53261abe674ee3191920ce5e7be736880157fa
vincenttuan <vincentjava@yahoo.com.tw>
2019-07-08 15:04:56 +0800

    python3

--- lesson12/Update.py
+++ lesson12/Update.py
@@ -4,7 +4,7 @@ from lesson12.Dao import getSession, User
 
 if __name__ == '__main__':
     session = getSession()
-    affected_rows = session.query(User).filter_by(id=4).update({'id': 4, 'name': 'sqlalchemy'})
+    affected_rows = session.query(User).filter_by(id=1).update({'id': 1, 'name': 'sqlalchemy'})
     print('Affected rows:', affected_rows)
 
     for r in session.query(User):
