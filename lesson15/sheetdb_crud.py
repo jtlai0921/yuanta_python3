b8f9eb495730ccfcf4bb29832119ee32e3a9b47e
vincenttuan <vincentjava@yahoo.com.tw>
2019-07-19 14:32:33 +0800

    python3

--- lesson15/sheetdb_crud.py
+++ lesson15/sheetdb_crud.py
@@ -7,4 +7,15 @@ def read():
     res = requests.get(url=url, headers=headers)
     print(res.text)
 
+def add():
+    data = '{"data":[{ "name": "Scott", "age": "21" }]}'
+    res = requests.post(url=url, data=data, headers=headers)
+
+def update():
+    data = '{"data":[{ "name": "Scott", "age": "40" }]}'
+    url = 'https://sheetdb.io/api/v1/pn0tjkupilpqk/name/Scott'
+    res = requests.patch(url=url, data=data, headers=headers)
+
+#add()
+update()
 read()
